8963ac2915a27e8e43606721c963ab95c159f017
TommasoAzz <azzalintommaso@gmail.com>
2021-08-25 20:24:18 +0200

    Formattato 03

--- 03_dbscan_clustering.py
+++ 03_dbscan_clustering.py
@@ -18,26 +18,6 @@ MAP_FILE_NAME: str = "land_limits" # "country_limits"
 # Distance threshold between points (to be considered in the same cluster)
 DISTANCE_THRESHOLD = 100
 
-def main():
-    stay_points = pd.read_csv(INPUT_FILE_STAYPOINTS)
-    X = pd.DataFrame(stay_points.drop(["time_of_arrival", "time_of_leave", "region_id", "user"], axis=1))
-    eps = (DISTANCE_THRESHOLD * 0.001) / 111
-
-    cluster_labels = DBSCAN(eps=eps, min_samples=5).fit_predict(X)
-
-    X = np.array(X)
-
-    # Map bounds
-    lon_min = -180.0
-    lon_max = 180.0
-    lat_min = -90.0
-    lat_max = 90.0
-
-    bounds = (lon_min, lon_max, lat_min, lat_max)
-
-    print(f"Silhouette Coefficient (should be in [-1,+1]): {metrics.silhouette_score(X, cluster_labels)}")
-
-    plot_points_and_map(cluster_labels, X, bounds)
 
 def plot_map():
     map_shp_file = open(MAP_FILE_NAME + '.shp', 'rb')
@@ -48,6 +28,7 @@ def plot_map():
         x = [i[0] for i in shape.shape.points[:]]
         y = [i[1] for i in shape.shape.points[:]]
         plt.plot(x, y)
+
 
 def plot_points_and_map(clusters: np.ndarray, X: np.array, bounds: Tuple[float, float, float, float]):
     plot_map()
@@ -77,9 +58,27 @@ def plot_points_and_map(clusters: np.ndarray, X: np.array, bounds: Tuple[float, 
     plt.show()
 
 
-if __name__ == '__main__':
-    main()
+def main():
+    stay_points = pd.read_csv(INPUT_FILE_STAYPOINTS)
+    X = pd.DataFrame(stay_points.drop(["time_of_arrival", "time_of_leave", "region_id", "user"], axis=1))
+    eps = (DISTANCE_THRESHOLD * 0.001) / 111
 
-def plot_statistics(X: np.array, cluster_labels: np.ndarray):
-    u_cluster_labels: np.ndarray = np.unique(cluster_labels)
-    
+    cluster_labels = DBSCAN(eps=eps, min_samples=5).fit_predict(X)
+
+    X = np.array(X)
+
+    # Map bounds
+    lon_min = -180.0
+    lon_max = 180.0
+    lat_min = -90.0
+    lat_max = 90.0
+
+    bounds = (lon_min, lon_max, lat_min, lat_max)
+
+    print(f"Silhouette Coefficient (should be in [-1,+1]): {metrics.silhouette_score(X, cluster_labels)}")
+
+    plot_points_and_map(cluster_labels, X, bounds)
+
+
+if __name__ == '__main__':
+    main()  
